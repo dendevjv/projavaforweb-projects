e0bac03412a67329261bfac956266ade31ab9cd9
dendevjv <dendevjv@yandex.ru>
2016-03-01 14:49:26 +0300

    Bootstrapping. Authentication, Index, SessionList Controllers

---
+++ customer-support-v10/src/main/java/projava4webbook/customer_support_v10/config/Bootstrap.java
@@ -0,0 +1,50 @@
+package projava4webbook.customer_support_v10.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import projava4webbook.customer_support_v10.site.AuthenticationFilter;
+import projava4webbook.customer_support_v10.site.LoggingFilter;
+
+public class Bootstrap implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+        container.getServletRegistration("default").addMapping("/resource/*");
+        
+        /* Configuration of ApplicationContexts. */
+        
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootContextConfiguration.class);
+        container.addListener(new ContextLoaderListener(rootContext));
+        
+        AnnotationConfigWebApplicationContext servletContext = new AnnotationConfigWebApplicationContext();
+        servletContext.register(ServletContextConfiguration.class);
+        ServletRegistration.Dynamic dispatcher = container.addServlet("springDispatcher", new DispatcherServlet(servletContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.setMultipartConfig(new MultipartConfigElement(
+                null, 20_971_520L, 41_943_040L, 512_000
+        ));
+        dispatcher.addMapping("/");
+        
+        /* Configuration of Filters. */
+        
+        FilterRegistration.Dynamic registration;
+        
+        registration = container.addFilter("loggingFilter", new LoggingFilter());
+        registration.addMappingForUrlPatterns(null, false, "/*");
+        
+        registration = container.addFilter("authenticationFilter", new AuthenticationFilter());
+        registration.addMappingForUrlPatterns(null, false, "/sessions", "/sessions/*", "/chat", "/chat/*", "/tickets",
+                "/tickets/*");
+    }
+
+}
---
+++ customer-support-v10/src/main/java/projava4webbook/customer_support_v10/config/RootContextConfiguration.java
@@ -0,0 +1,34 @@
+package projava4webbook.customer_support_v10.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Controller;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Configuration
+@ComponentScan(
+        basePackages = "projava4webbook.customer_support_v10.site", 
+        excludeFilters = @ComponentScan.Filter(Controller.class) 
+)
+public class RootContextConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.findAndRegisterModules();
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
+        return mapper;
+    }
+
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setPackagesToScan(new String[] { "com.wrox.site" });
+        return marshaller;
+    }
+}
